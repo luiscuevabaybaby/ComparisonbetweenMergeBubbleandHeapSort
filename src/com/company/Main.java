6013de8aec5e7b5446d384059439d368607b98e4
Luis Cueva <luis.cuevas@ujcv.edu.hn>
2019-03-20 19:10:11 -0600

    merge sort vs bubble sort vs heap sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,37 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Timer;
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
 
-        int[] arreglo1 = new int[100];
-        int[] arreglo2 = new int[100];
+        int[] arreglo1 = new int[10];
+        int[] arreglo2 = new int[10];
+        int[] arreglo3 = new int[10];
 
 
 
         for (int i = 0; i<arreglo1.length;i++){
-            int temp = (int)(Math.random()*99)+1;
+            int temp = (int)(Math.random()*10)+1;
             arreglo1[i] = temp;
             arreglo2[i] = temp;
+            arreglo3[i] = temp;
         }
 
         arreglo1 = mergeSort(arreglo1);
         arreglo2 = bubbleSort(arreglo1);
 
+
+
         Timer timer = new Timer();
+        long Time = System.currentTimeMillis();
 
         System.out.println("-----------------MERGE SORT-----------------");
-        for (int i = 0; i < arreglo1.length; i++) {
-            long Time = System.currentTimeMillis();
+       for (int i = 0; i < arreglo1.length; i++) {
+
             System.out.print(arreglo1[i]+", ");
             System.out.println(Time+" Miliseconds");
 
@@ -37,7 +44,35 @@ public class Main {
             System.out.print(arreglo2[i]+", ");
             System.out.println(TIME+" Miliseconds");
         }
-    }
+
+
+        System.out.println("----------------------HEAP SORT---------------");
+        System.out.println("Antes de Heap Sort");
+
+            System.out.print(Arrays.toString(arreglo3));
+            System.out.println(Time+" Miliseconds");
+
+        System.out.println("Despues de Heap sort.");
+            long TIME = System.currentTimeMillis();
+            arreglo3 = heapSort(arreglo3);
+            System.out.print(Arrays.toString(arreglo3));
+            System.out.println(TIME+" Miliseconds");System.out.println();
+
+        System.out.println("Merge Sort Arreglado");
+
+        System.out.print(Arrays.toString(arreglo1));
+        System.out.println(Time+" Miliseconds");
+
+
+        System.out.println("");
+        System.out.println("Bubble Sort Arreglado");
+
+        System.out.print(Arrays.toString(arreglo2));
+        System.out.println(Time+" Miliseconds");
+        }
+
+
+
 
 
     public static int[] bubbleSort(int[] arr){
@@ -50,6 +85,50 @@ public class Main {
                     arr[j] = temp;
                 }
             }
+        }
+        return arr;
+    }
+    public static void armarheap(int []arr) {
+
+        for(int i=(arr.length-1)/2; i>=0; i--){
+            heapify(arr,i,arr.length-1);
+        }
+    }
+
+    public static void heapify(int[] arr, int i,int size) {
+        int left = 2*i+1;
+        int right = 2*i+2;
+        int max;
+        if(left <= size && arr[left] > arr[i]){
+            max=left;
+        } else {
+            max=i;
+        }
+
+        if(right <= size && arr[right] > arr[max]) {
+            max=right;
+        }
+
+        if(max!=i) {
+            intercambiar(arr,i, max);
+            heapify(arr, max,size);
+        }
+    }
+
+    public static void intercambiar(int[] arr,int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    public static int[] heapSort(int[] arr) {
+
+        armarheap(arr);
+        int sizeOfHeap = arr.length - 1;
+        for (int i = sizeOfHeap; i > 0; i--) {
+            intercambiar(arr, 0, i);
+            sizeOfHeap = sizeOfHeap - 1;
+            heapify(arr, 0, sizeOfHeap);
         }
         return arr;
     }
